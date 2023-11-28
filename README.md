4924dd5c2a0c2d7c93a3c01ba6b072ad0ef7e3cf
yuanzero <827006148@qq.com>
2023-11-28 19:09:36 +0800

    add arduino code

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is a HoloLens2-MRTK-ready template, with TCP and UDP web modules integrated, based on:
  
-    ·Unity 2020.3.3f1(LTS)     
+    ·Unity 2020.3.42f1(LTS)     
     ·OpenXR features    
     ·Windows Mixed Reality Toolkit (MRTK) 2.7.2
     
@@ -12,18 +12,14 @@ For the users:
     ·Step 1. download it and deploy this project to HoloLens2 directly.
     ·Step 2. In Unity Editor, go to the top and click : Mixed Reality--> Toolkit --> Untilities --> Configure Project for MRTK --> Apply Settings, this step will help your project re-configure for MRTK and avoid errors.
     ·Step 3. In Unity Editor, find the scene "SampleScene" in the folder "Scenes", in this scene you can find the gameobject "UDP Communication" as an example
-    ·Step 4. f_Init() is the initialization function in both TCP and UDP scripts. In my project, I use a globalCtrl script to call this function and then activate the web connection; but for you, you can directly change the name of this function to be "Start()", so it will start automatically.
+    ·Step 4. in Arduino editor runnning UDP_test.ino, setting your wifi and IP. upload it to ESP32.
 
 One finding is that the TCP connection reads too slowly, I don't know why. I will be very happy if one day you let me know that you create a better TCP connection.
 
 Tips: 
 
-    ·Step 1. You had better name the solution folder as "APP" when you build this project from Unity, because I have already add this name in the gitignore file             
-    ·Step 2. Give me a Star please, this took me 2 full weeks because I am not a professional programmer.
+    ·Step 1. The hololens2 and unity have different codes since they are different platforms. It is hard to debug.          
+    ·Step 2. The code has some precompiled code, be careful.
 
-
-
-
-xxxhhhyxy
-
-03.11.2021
+zhongyuan liao
+2023.11.28
---
+++ UDP_test/UDP_test.ino
@@ -0,0 +1,223 @@
+#include <WiFi.h>
+#include <WiFiUdp.h>
+
+const char* ssid = "SMMG_CAlgroup";
+const char* password = "hkustsmmg";
+
+WiFiUDP udp;
+IPAddress serverIP;
+unsigned int inPort;
+unsigned int outPort;
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+
+  Serial.println("Connected to WiFi");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  serverIP = IPAddress(192, 168, 1, 12); // 设置Hololens服务器的IP地址
+  inPort = 22222; // 设置接收消息的端口号
+  outPort = 22224; // 设置发送消息的端口号
+
+  udp.begin(inPort);
+  Serial.print("Listening on UDP port ");
+  Serial.println(inPort);
+}
+
+
+void SetMode(char* packetData) {
+  int pinToMap = 100; //100 is never reached
+  char *arg = strtok(packetData, " ");
+  arg = strtok(NULL, " ");
+  if (arg != NULL)
+  {
+    pinToMap = atoi(arg);
+  }
+  int type;
+  arg = strtok(NULL, " ");
+  if (arg != NULL)
+  {
+    type = atoi(arg);
+    PinSetMode(pinToMap, type);
+  }
+
+  Serial.println( "PinMOde set" );
+}
+
+void PinSetMode(int pin, int type) {
+  //TODO : vérifier que ça, ça fonctionne
+  if (type != 4)
+    //DisconnectServo(pin);  //uncommon initial
+
+  switch (type) {
+    case 0: // Output
+      pinMode(pin, OUTPUT);
+      break;
+    case 1: // PWM
+      pinMode(pin, OUTPUT);
+      break;
+    case 2: // Analog
+      pinMode(pin, INPUT);
+      break;
+    case 3: // Input_Pullup
+      pinMode(pin, INPUT_PULLUP);
+      break;
+    case 4: // Servo
+      //SetupServo(pin);
+      break;
+  }
+}
+
+void WritePinAnalog(char* packetData) {
+  int pinToMap = 100;
+  char *arg = strtok(packetData, " ");
+  arg = strtok(NULL, " ");
+  if (arg != NULL)
+  {
+    pinToMap = atoi(arg);
+  }
+
+  int valueToWrite = 0;
+  arg = strtok(NULL, " ");
+  if (arg != NULL)
+  {
+    valueToWrite = atoi(arg);
+  }
+
+  analogWrite(pinToMap, valueToWrite);
+}
+
+void WritePinDigital(char* packetData) {
+  int pinToMap = -1;
+  char *arg = strtok(packetData, " ");
+  arg = strtok(NULL, " ");
+  if (arg != NULL)
+    pinToMap = atoi(arg);
+
+  int writeValue;
+  arg = strtok(NULL, " ");
+  if (arg != NULL && pinToMap != -1)
+  {
+    writeValue = atoi(arg);
+    
+    if (writeValue == 0)
+    {
+      digitalWrite(pinToMap, LOW);
+      Serial.println( pinToMap );
+      Serial.println( "is low");
+      Serial.println( "writeValue is" );
+      Serial.println( writeValue );
+      
+     digitalWrite(pinToMap, LOW);
+    }
+    else
+    {
+      digitalWrite(pinToMap, HIGH);
+      Serial.println( pinToMap );
+      Serial.println( "is high");
+      Serial.println( "writeValue is" );
+      Serial.println( writeValue );
+      
+      digitalWrite(pinToMap, HIGH);
+    }
+    
+  }
+}
+
+void ReadAnalogPin(char* packetData) {
+  int pinToRead = -1;
+  char *arg = strtok(packetData, " ");
+  arg = strtok(NULL, " ");
+  if (arg != NULL)
+  {
+    pinToRead = atoi(arg);
+    if (pinToRead != -1)
+      analogRead(pinToRead);
+  }
+  
+  String message = "readpin " + String(pinToRead) + " " + analogRead(pinToRead) ;
+  udp.beginPacket(serverIP, outPort);
+  udp.print(message);
+  udp.endPacket();
+  Serial.print( "read is: " );
+  Serial.println( analogRead(pinToRead) );
+}
+
+void ReadDigitalPin(char* packetData) {
+  int pinToRead = -1;
+  char *arg = strtok(packetData, " ");
+  arg = strtok(NULL, " ");
+  if (arg != NULL)
+  {
+    pinToRead = atoi(arg);
+  }
+  if (pinToRead != -1)
+    digitalRead(pinToRead);
+}
+
+void BundleReadPin(char* packetData) {
+  int pinToRead = -1;
+  char *arg = strtok(packetData, " ");
+  arg = strtok(NULL, " ");
+  if (arg != NULL)
+  {
+    pinToRead = atoi(arg);
+    if (pinToRead != -1)
+      analogRead(pinToRead);
+  }
+}
+
+void loop() {
+  int packetSize = udp.parsePacket();
+  if (packetSize) {
+    Serial.print("Received packet of size ");
+    Serial.println(packetSize);
+    char packetData[255];
+    int len = udp.read(packetData, 255);
+    if (len > 0) {
+      packetData[len] = 0;
+    }
+    Serial.print("Received message: ");
+    Serial.println(packetData);
+    
+    delay(1000);
+
+    // 解析命令并执行相应的操作
+  if (packetData[0] == 's') {
+    SetMode(packetData);
+  } else if (packetData[0] == 'd') {
+    WritePinDigital(packetData);
+  } else if (packetData[0] == 'a') {
+    WritePinAnalog(packetData);
+  } else if (packetData[0] == 'rd') {
+    ReadDigitalPin(packetData);
+  } else if (packetData[0] == 'r') {
+    ReadAnalogPin(packetData);
+  } else if (packetData[0] == 'br') {
+    BundleReadPin(packetData);
+  } else if (packetData[0] == 'b') {
+    
+  } else {
+    // 未知命令处理
+    Serial.println("Unknown command");
+  }
+  
+  }else{
+ 
+  String message = "Hello from ESP32!";
+  udp.beginPacket(serverIP, outPort);
+  udp.print(message);
+  udp.endPacket();
+  Serial.println("Sent message: " + message);
+
+  delay(5000);
+  }
+}
